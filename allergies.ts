4dcdb204a1edc7325e5639f93e91aeafd0351ae1
Victor Fernandez <vfernandezInnovation>
2022-03-30 09:07:11 +0200

    31 lineas

--- allergies.ts
+++ allergies.ts
@@ -1,57 +1,31 @@
 export class Allergies {
-  score:number
   listAllAllergies = new Map<string, number>();
   listAllergies = new Map<string, number>();
   constructor(allergenIndex: number) {
-    this.score = allergenIndex;
-    this.listAllAllergies.set("eggs", 1);
-    this.listAllAllergies.set("peanuts", 2);
-    this.listAllAllergies.set("shellfish", 4);
-    this.listAllAllergies.set("strawberries", 8);
-    this.listAllAllergies.set("tomatoes", 16);
-    this.listAllAllergies.set("chocolate", 32);
-    this.listAllAllergies.set("pollen", 64);
     this.listAllAllergies.set("cats", 128);
-
-    
-    let scoreTmp:number = this.score;
-    while (scoreTmp > 0){
-      let aliment:string = getAliment(scoreTmp, this.listAllAllergies)
-      let scoreAliemnt = this.listAllAllergies.get(aliment)
-      if(scoreAliemnt != null){
-         scoreTmp = scoreTmp - scoreAliemnt
-        this.listAllergies.set(aliment,  scoreAliemnt);
-        console.log(this.listAllergies)
+    this.listAllAllergies.set("pollen", 64);
+    this.listAllAllergies.set("chocolate", 32);
+    this.listAllAllergies.set("tomatoes", 16);
+    this.listAllAllergies.set("strawberries", 8);
+    this.listAllAllergies.set("shellfish", 4);
+    this.listAllAllergies.set("peanuts", 2);
+    this.listAllAllergies.set("eggs", 1);
+    while (allergenIndex > 0) {
+      for (var [key, value] of this.listAllAllergies) {
+        if (value <= allergenIndex) {
+          allergenIndex = allergenIndex - value;
+          this.listAllergies.set(key, value);
+          break;
+        }
       }
     }
   }
   public list(): string[] {
-    let array:string[] = []
-    for (let [key, value] of this.listAllergies) {
-      array.push(key);
-    }
-    console.log(array)
+    let array: string[] = [];
+    for (let [key] of this.listAllergies) array.push(key);
     return array.reverse();
   }
   public allergicTo(allergen: string): boolean {
-    return this.listAllergies.has(allergen)
+    return this.listAllergies.has(allergen);
   }
 }
-function getAliment(score:number, map:Map<string, number>):string{
-  let valorAnterior:string = ""
-  for (var [key, value] of map) {
-    if(score >= 128) return "cats"
-    if(value == score){
-      return key;
-    }
-    else if(value > score){
-      return valorAnterior;
-    }
-    valorAnterior = key;
-  }
-  return "null";
-}
-
-
-
-
